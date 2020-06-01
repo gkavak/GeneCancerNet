d18d40b2173c07079b7f84f3c904e2e8fa4e1ce1
Güven <gkavak@gmail.com>
2020-06-01 11:39:58 +0300

    initial commit

---
+++ app/src/main/java/com/guvenkavak/genecancernet/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.guvenkavak.genecancernet.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.guvenkavak.genecancernet.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
